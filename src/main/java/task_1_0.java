4396d0c0749adf4d34ebc23559a3fde8c8a40635
RomanShmakov <125243379+RomanShmakov@users.noreply.github.com>
2024-06-28 18:23:09 +0300

    Update task_1_0.java

--- src/main/java/task_1_0.java
+++ src/main/java/task_1_0.java
@@ -1,7 +1,35 @@
 public class task_1_0 {
 
-    public void hello() {
-        System.out.println("hello");
+    public void binary_search() {
+
+        List<Integer> my_list = Arrays.asList(1, 3, 5, 7, 9);
+        int item = 5;
+
+        int low = 0;
+        int high = my_list.size() - 1;
+        int result = -1;
+        int mid;
+        int guess;
+        while (low <= high) {
+            mid = (low + high) / 2;
+            guess = my_list.get(mid);
+            if (guess == item) {
+                result = mid;
+                break;
+            } else if (guess > item) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        if (result >= 0) {
+            System.out.println("list[" + result + "]:" + item);
+        } else {
+            System.out.println("NO NUMBER");
+
+        }
+
     }
 
 }
